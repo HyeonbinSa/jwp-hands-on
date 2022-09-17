873200084909c084da5eb7f4e4bae8794199c987
hyeonbinsa <shb1833@gmail.com>
2022-09-17 13:53:57 +0900

    feat: Junit4TestRunner 구현

--- README.md
+++ README.md
@@ -47,7 +47,7 @@
 
 ### 자바 리플렉션
 - [x] Junit3TestRunner 클래스의 모든 테스트를 통과시킨다.
-- [ ] Junit4TestRunner 클래스의 모든 테스트를 통과시킨다.
+- [x] Junit4TestRunner 클래스의 모든 테스트를 통과시킨다.
 - [x] ReflectionTest 클래스의 모든 테스트를 통과시킨다.
 - [ ] ReflectionsTest 클래스의 모든 테스트를 통과시킨다.
 
--- reflection/src/test/java/reflection/Junit4TestRunner.java
+++ reflection/src/test/java/reflection/Junit4TestRunner.java
@@ -1,5 +1,9 @@
 package reflection;
 
+import java.lang.reflect.Method;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 import org.junit.jupiter.api.Test;
 
 class Junit4TestRunner {
@@ -7,7 +11,13 @@ class Junit4TestRunner {
     @Test
     void run() throws Exception {
         Class<Junit4Test> clazz = Junit4Test.class;
-
+        Junit4Test junit4Test = clazz.getDeclaredConstructor().newInstance();
         // TODO Junit4Test에서 @MyTest 애노테이션이 있는 메소드 실행
+        List<Method> methods = Arrays.stream(clazz.getDeclaredMethods())
+                .filter(method -> method.isAnnotationPresent(MyTest.class))
+                .collect(Collectors.toList());
+        for (Method method : methods) {
+            method.invoke(junit4Test);
+        }
     }
 }
